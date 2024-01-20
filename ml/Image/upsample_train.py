7106932726a84dc5ae6c0798404729fc121af276
steinshark <everettperson@gmail.com>
2024-01-20 16:48:31 -0500

    cleaned

--- ml/Image/upsample_train.py
+++ ml/Image/upsample_train.py
@@ -79,12 +79,60 @@ def apply_distortion(img:torch.Tensor,start_dim:int=48,noise_sf:float=.1,noise_i
     return downscaled,img
 
 
+def tfloat_to_np(x:torch.Tensor)->numpy.ndarray:
+    
+    #convert to 1-255
+    x           = x + 1 
+    x           = x / 2
+    x           = x * 255
+    x           = x.int()
+    x           = x.numpy()
+    x           = numpy.transpose(x,[1,2,0])
+    return x 
+
+
+def make_grid(x:torch.Tensor,y:torch.Tensor,ep:int,batch:int)->None:
+
+    #Take first 3 imgs 
+    width       = 2* y.shape[-2]
+    height      = 3* y.shape[-1]
+
+    #Refurbish1 
+    x1              = x[0].unsqueeze_(dim=0)
+    x1              = torch.nn.functional.interpolate(x1,scale_factor=4)[0]
+    refurbished1    = torch.ones(size=(y.shape[1:]))
+    refurbished1[:,:x1.shape[1],:x1.shape[2]]    = x1
+    row1            = torch.cat([refurbished1,y[0]],dim=1)
+
+    #Refurbish2
+    x2              = x[1].unsqueeze_(dim=0)
+    x2              = torch.nn.functional.interpolate(x2,scale_factor=4)[0]
+    refurbished2    = torch.ones(size=(y.shape[1:]))
+    refurbished2[:,:x2.shape[1],:x2.shape[2]]    = x2
+    row2            = torch.cat([refurbished2,y[1]],dim=1)
+
+    #Refurbish3
+    x3              = x[2].unsqueeze_(dim=0)
+    x3              = torch.nn.functional.interpolate(x3,scale_factor=4)[0]
+    refurbished3    = torch.ones(size=(y.shape[1:]))
+    refurbished3[:,:x3.shape[1],:x3.shape[2]]    = x3
+    row3            = torch.cat([refurbished3,y[2]],dim=1)
+
+    finalimg        = torch.cat([row1,row2,row3],dim=2)
+
+
+
+    plt.imshow(tfloat_to_np(finalimg))
+    plt.savefig(f"C:/code/projects/ml/Image/outs/ep{ep}_b{batch}")
+
+    
 def train(ds_path,n_ep,bs):
 
     #LOCAL OPTIONS
     start_dim           = 48 
     max_distorts        = 4
     max_strength        = .1
+    max_n               = 256
 
     #TRAIN OPTIONS 
     lr                  = 1e-4
@@ -99,7 +147,7 @@ def train(ds_path,n_ep,bs):
     loss_fn             = torch.nn.MSELoss()
 
     #Prepare data
-    dataloader      =   load_locals(bs=bs,processor=None,max_n=128)
+    dataloader      =   load_locals(bs=bs,processor=None,max_n=max_n)
     
     #Stats
     losses              = []
@@ -119,8 +167,7 @@ def train(ds_path,n_ep,bs):
             base_img    = item.to(DEV).type(torch.float)
 
             #Apply distortion
-            x,y         = apply_distortion(base_img)    
-
+            x,y         = apply_distortion(base_img,noise_sf=random.random()*max_strength,noise_iters=random.randint(0,max_distorts))    
             #Send thorugh model 
             pred        =  model.forward(x)
 
@@ -131,8 +178,9 @@ def train(ds_path,n_ep,bs):
 
             #Grad 
             optim.step()
-
+            make_grid(x,pred,ep,batch_i)
             print(f"\tbatch loss={losses[-1][-1]:.5f}")
+            
 
 if __name__ == '__main__':
 
